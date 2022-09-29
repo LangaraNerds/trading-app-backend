b6d1ab3d1efd7be57c007a51157052c0c41ee789
otakuchan <ianbjs@gmail.com>
2022-09-29 02:40:34 -0700

    CAP-8 Connect to MongoDB and created user endpoints

--- server/server.js
+++ server/server.js
@@ -3,7 +3,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const colors = require("colors");
+const morgan = require("morgan");
+
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./database/connection");
+
 const PORT = process.env.PORT || 8080;
+
+// Connect to database
+connectDB();
 
 /* This is a middleware that allows the server to accept requests from a different origin. */
 const corsOptions = {
@@ -17,6 +26,14 @@ app.use(express.urlencoded({extended: true}));
 /* Listening to the port that is set in the environment. */
 app.listen(PORT, () => console.log(`listening to ${PORT}`));
 
+// HTTP request logger
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to Team Psyduck API." });
 });
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
