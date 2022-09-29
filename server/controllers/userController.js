74b015e9acf0ab864bf1a1b2ded2da0a3a9739ad
otakuchan <ianbjs@gmail.com>
2022-09-29 03:35:13 -0700

    CAP-8 Add user login endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,29 +6,28 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 // @desc   Register a new user
-// @route  /api/users
+// @route  /api/users/signup
 // @access Public
 const userSignup = asyncHandler(async (req, res) => {
-
-    const {firstName, lastName, email, password} = req.body
+    const { firstName, lastName, email, password } = req.body;
 
     //Validation
-    if(!firstName || !lastName || !email || !password) {
-        res.status(400)
-        throw new Error('Please include all fields')
+    if (!firstName || !lastName || !email || !password) {
+        res.status(400);
+        throw new Error("Please include all fields");
     }
 
     // Check if user mail already exists
-    const userExist = await User.findOne({ email })
+    const userExist = await User.findOne({ email });
 
-    if(userExist) {
-        res.status(400)
-        throw new Error('User already exists')
+    if (userExist) {
+        res.status(400);
+        throw new Error("User already exists");
     }
 
     // Password encyptions
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     // Create user
     const user = await User.create({
@@ -36,9 +35,33 @@ const userSignup = asyncHandler(async (req, res) => {
         lastName,
         email,
         password: hashedPassword,
-    })
+    });
 
-    if(user) {
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+});
+
+// @desc   Login a new user
+// @route  /api/users/login
+// @access Public
+const userLogin = asyncHandler(async (req, res) => {
+
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    // Check user and password match
+    if(user && (await bcrypt.compare(password, user.password))){
         res.status(201).json({
             _id: user._id,
             firstName: user.firstName,
@@ -47,11 +70,11 @@ const userSignup = asyncHandler(async (req, res) => {
             token: generateToken(user._id)
         })
     } else {
-        res.status(400)
-        throw new Error('Invalid user data')
+        res.status(401)
+        throw new Error('Invalid username and password. Please try again.')
     }
 
-});
+})
 
 // Generate Token
 const generateToken = (id) => {
@@ -62,4 +85,5 @@ const generateToken = (id) => {
 
 module.exports = {
     userSignup,
+    userLogin
 };
