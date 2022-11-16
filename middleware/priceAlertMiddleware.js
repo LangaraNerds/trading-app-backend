b5e060a981c57fe184f21c766efcd810c7d87b7f
Gabriel Cordeiro <gpcordeiro@hotmail.com>
2022-11-16 12:43:34 -0800

    CAP-127 middlwares for push notification

--- middleware/priceAlertMiddleware.js
+++ middleware/priceAlertMiddleware.js
@@ -1,18 +1,46 @@
-const PriceAlert = require("../models/priceAlertModel");
 const axios = require("axios");
-const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
-const {pushNotification} = require('../middleware/pushNotificationMiddleware')
+const User = require("../models/userModel");
+const PriceAlert = require("../models/priceAlertModel");
+const {ToadScheduler, SimpleIntervalJob, Task} = require('toad-scheduler')
+const {pushNotification} = require('./pushNotificationMiddleware');
 
 
-const { ToadScheduler, SimpleIntervalJob, Task } = require('toad-scheduler')
+exports.AlertTasks = () => {
+    const scheduler = new ToadScheduler()
 
-const scheduler = new ToadScheduler()
+    const dogeAlert = new Task('DOGEUSDT Alert', async () => {
+        console.log('dogeAlert');
+        const coinAlerts = await PriceAlert.find({ticker: "DOGEUSDT"});
+        let usersId = []
+        if (coinAlerts) {
+            const coinFetch = await axios.get(`https://api.binance.com/api/v3/ticker/price?symbol=DOGEUSDT`);
+            const coinJson = coinFetch.data;
+            const coinPrice = coinJson.price;
+            console.log(`market-price: ${coinPrice}`)
 
-const task = new Task('simple task', () => { counter++ })
-const job = new SimpleIntervalJob({ seconds: 20, }, task)
+            for (const alertPrice of coinAlerts) {
+                    console.log(`alert-price ${alertPrice.price}`)
+                if (coinPrice === alertPrice.price && alertPrice.notified === false) {
+                    usersId.push(alertPrice.user_id)
+                }
+            }
+            pushNotification(usersId, coinPrice, "DOGE", "DOGEUSDT");
+        }
 
-scheduler.addSimpleIntervalJob(job)
+    })
+    const dogeJob = new SimpleIntervalJob({seconds: 30,}, dogeAlert, {id: 'doge'})
 
-// when stopping your app
-scheduler.stop()
+
+    scheduler.addSimpleIntervalJob(dogeJob)
+
+    console.log(scheduler.getById('doge').getStatus());
+
+}
+
+exports.stopTasks = () => {
+    const scheduler = new ToadScheduler()
+    scheduler.stop()
+}
+
+
--- routes/cryptoInfoRoutes.js
+++ routes/cryptoInfoRoutes.js
@@ -1,12 +1,22 @@
 const router = require("express").Router();
-const {coinSingle, trendingCoins, priceAlert} = require("../controllers/cryptoInfoController")
-
-const {isAuthenticated} = require('../middleware/authMiddleware')
-
+const {coinSingle, trendingCoins, priceAlert} = require("../controllers/cryptoInfoController");
+const PriceAlert = require("../models/priceAlertModel");
+const {isAuthenticated} = require('../middleware/authMiddleware');
+const priceNotification = require("../middleware/priceAlertMiddleware");
 module.exports = function (app) {
 
     router.post("/", coinSingle);
     router.get("/trending", trendingCoins);
-    router.post("/alert", priceAlert)
+    router.post("/alert", priceAlert);
+    router.get('/test', async (req, res) => {
+        let arr = []
+        const coin = await PriceAlert.find({ticker: "DOGEUSDT"})
+        for (alert of coin){
+        console.log(alert.price)
+            arr.push(alert.price)
+        }
+console.log(arr)
+        res.send({})
+    })
     app.use('/api/crypto', router)
 }
