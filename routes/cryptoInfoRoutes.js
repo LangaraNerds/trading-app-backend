9e9bb5fd9223e62e759def8bc8e8dafba2410897
Gabriel Cordeiro <gpcordeiro@hotmail.com>
2022-11-23 17:28:27 -0800

    CAP-13 get all the alerts of the user

--- routes/cryptoInfoRoutes.js
+++ routes/cryptoInfoRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const {coinSingle, trendingCoins, priceAlert, orderLimit} = require("../controllers/cryptoInfoController");
+const {coinSingle, trendingCoins, priceAlert, orderLimit, alertsInfo} = require("../controllers/cryptoInfoController");
 
 const {isAuthenticated} = require('../middleware/authMiddleware');
 module.exports = function (app) {
@@ -7,6 +7,7 @@ module.exports = function (app) {
     router.post("/", coinSingle);
     router.get("/trending", trendingCoins);
     router.post("/alert", priceAlert);
+    router.post("/alert/info", alertsInfo);
     router.post("/order", orderLimit);
 
     app.use('/api/crypto', router)
