3aa7355fb53d2ba9206071b5dac7c2e4ebc8502d
otakuchan <ianbjs@gmail.com>
2022-12-02 06:09:51 -0800

    Feat: get top 10 traders

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -3,7 +3,7 @@ const Asset = require("../models/assetsModel");
 const BuyHistory = require("../models/buyHistoryModel")
 const SellHistory = require("../models/sellHistoryModel")
 const {fetchPrice} = require("../utils/APIs");
-
+const lodash = require("lodash");
 const asyncHandler = require("express-async-handler");
 const axios = require("axios");
 
@@ -17,7 +17,7 @@ exports.topTraders = asyncHandler(async ({body}, res) => {
     const { city } = body
     try { 
 
-        console.log("City", city)
+        // console.log("City", city)
 
         // get all users in the city and their assets
         let aggregateQuery = User.aggregate([
@@ -26,46 +26,73 @@ exports.topTraders = asyncHandler(async ({body}, res) => {
                     "location.city": {
                         $in: [city]
                     }
+                    // "email": {
+                    //     $in: ["chantour@gmail.com"]
+                    // }
                 }
             },
             {
                 $lookup: {
                     from: "assets",
-                    localField: "user_id",
-                    foreignField: "firebase_uuid",
+                    localField: "firebase_uuid",
+                    foreignField: "user_id",
                     as: "assets"
                 }
             },
-            {
-                $group: {
-                    // '_id': '$users._id',
-                    'users_count': {
-                        '$sum': {
-                            '$cond': [{'$eq': ['$users.role', 'user']}, 1, 0]
-                        }
-                    },
-                    // 'volunteer_count'   : {
-                    //     '$sum': {
-                    //         '$cond': ['$users.isVolunteer', 1, 0]
-                    //     }
-                    // },
-                    // 'pet_count': { '$sum': 1 },
-                    // 'lost_pets': {
-                    //     '$sum': {
-                    //         '$cond': ['$petLost.lost', 1, 0]
-                    //     }
-                    // }       
+        ])
+        // aggregateQuery.exec(function(err,res){
+        //     console.log("res", res)
+        // })
+
+        let totalBalance = 0
+        let assetBalance = 0
+        let performance = 0
+        let topArr = []
+
+        for await (const doc of aggregateQuery) {
+
+            const buy = await BuyHistory.find({user_id: doc.firebase_uuid})
+            const sell = await SellHistory.find({user_id: doc.firebase_uuid})
+
+            let totalTrades = buy.length + sell.length
+            
+            // Minimum of 10 trades
+            if(totalTrades > 2){
+                let newassets = lodash.groupBy(doc.assets, 'ticker')
+            
+                for (const [key, value] of Object.entries(newassets)) {
+                    // console.log(`${key}: ${value}`);
+                    let totalQuantity = 0
+                    for (const asset of value) {
+                        totalQuantity = totalQuantity + asset.quantity
+                    }
+                    newassets[key] = totalQuantity
                 }
+    
+                for (const [key, value] of Object.entries(newassets)) {
+                    const coinPrice = await fetchPrice(key)
+                    assetBalance = assetBalance + (value * coinPrice)
+                    totalBalance = doc.wallet.balance + assetBalance
+                }
+                
+                // Calculate score by dividing performance by total trades
+                performance = ((parseFloat(totalBalance) - 1000) / 1000 * 100) / totalTrades
+    
+                topArr.push({
+                    email: doc.email,
+                    performance: performance
+                })
             }
-        ]);
-        aggregateQuery.exec(function(err,res){
-            console.log(res);
-        })
 
+        }
 
+        // Sort by performance and return top 10
+        topArr.sort((a, b) => (a.performance > b.performance) ? -1 : 1)
+        topArr = topArr.slice(0, 10)
 
+        // console.log("TopArr", topArr)
 
-        res.status(200).json({ success: true, user: aggregateQuery, message: "Top 10 users" });
+        res.status(200).json({ success: true, traders: topArr, message: "Top 10 users" });
     } catch (error) {
         res.status(500).json({success: false, message: error.message});
     }
