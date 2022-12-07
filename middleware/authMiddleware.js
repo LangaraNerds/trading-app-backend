17152acff8685d806a59753aa9d7024e7b5ddfd3
otakuchan <ianbjs@gmail.com>
2022-12-07 09:27:13 -0800

    refresh firebase id token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,10 +4,12 @@ const User = require("../models/userModel");
 const { getAuth } = require("firebase-admin/auth");
 
 const isAuthenticated = asyncHandler(async (req, res, next) => {
+	const uid = req.headers["uid"];
+	console.log(uid)
 	const regex = /Bearer (.+)/i;
 	try {
 		const idToken = req.headers['authorization'].match(regex)?.[1];
-
+		// console.log(idToken)
 		req.token = await getAuth().verifyIdToken(idToken);
 
 		next();
